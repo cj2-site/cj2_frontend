7f05523d97b187a49b5396b7237c02d82ed15c82
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-14 13:07:26 -0700

    fix: minor fixes. excessive declarations

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './header.jsx';
 import Home from './home.jsx';
 import Faq from './faq.jsx';
