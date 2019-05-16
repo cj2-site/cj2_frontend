ea8dbecf5e214cd7ad2a5b949fe79240a537ad95
Charles Clemens <cclemensjr@gmail.com>
2019-05-16 13:20:53 -0700

    Updated some divs

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -8,24 +8,24 @@ const Faq = () => {
           <h2>Frequently Asked Questions</h2>
         </div>
         <div>
-          <span>Q: How to use?</span>
+          <span>Q: How do I use this thing in all of it's glory?</span>
           <br/>
           <span>A: Enter your long URL and we give you back a short URL!</span>
         </div>
         <div>
-          <span>Q: Can I save my links?</span>
+          <span>Q: But... can I save this magical links you have create?</span>
           <br/>
-          <span>A: Yes! The links you made are automatically saved locally to your browser</span>
+          <span>A: Yes! The links you made are automatically saved to your browser</span>
         </div>
         <div>
-          <span>Q: Where are my links?</span>
+          <span>Q: But... where do they dwell?</span>
           <br/>
-          <span>A: Click on Links in the Nav bar up top</span>
+          <span>A: Simply click on "Links" at the top of the page!</span>
         </div>
         <div>
-          <span>Q: What happens if I lost a link?</span>
+          <span>Q: But... what happens if I lost a link?</span>
           <br/>
-          <span>A: Resubmit it and we will give you the previous shortened URL!</span>
+          <span>A: Resubmit the original URL and your link will appear before thine eyes!</span>
         </div>
       </section>
     </div>
