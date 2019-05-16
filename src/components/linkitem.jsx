207b9088ecf6fccebedb15d99e924eb02790acee
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-16 13:27:05 -0700

    feat: added copy button to links list

--- src/components/linkitem.jsx
+++ src/components/linkitem.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const If = props => {
+  return props.condition ? props.children : null;
+}
+
 export class LinkItem extends React.Component {
+
+  state = {
+    copied: false
+  }
+
+  copiedAnimation = () => {
+    this.setState({ copied: true });
+    this.timer = setTimeout(_ => {
+      this.setState({copied: false});
+    }, 2000);
+  }
+
+  copyToClipBoard = () => {
+    const el = document.createElement('textarea');
+    el.value = this.props.tinyURL;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    this.copiedAnimation();
+  }
 
   render() {
     return (
@@ -10,11 +35,12 @@ export class LinkItem extends React.Component {
           <img src={this.props.qrCode} alt="QR code for website"></img>
         </div>
         <div className="itemURLS">
-          <span>Short: { this.props.tinyURL }</span>
+          <span>Short: { this.props.tinyURL }</span><If condition={this.state.copied}><span style={{ color: '#32CD32' }}> Copied!</span></If>
           <br/>
           <span>Long: { this.props.longURL }</span>
         </div>
         <div className="itemButtons">
+          <button className="linkCopy" onClick={this.copyToClipBoard}>Copy URL</button>
           <button onClick={this.props.delLink.bind(this, this.props.tinyURL)}>Delete</button>
         </div>
       </div>
