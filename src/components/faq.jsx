5adf41036df43e58e64a5654a119b20235da5a7c
Charles Clemens <cclemensjr@gmail.com>
2019-05-16 20:16:21 -0700

    added simple styles to FAQ

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -8,24 +8,24 @@ const Faq = () => {
           <h2>Frequently Asked Questions</h2>
         </div>
         <div>
-          <span>Q: How do I use this thing in all of it's glory?</span>
+          <span>How do I use this thing in all of it's glory?</span>
           <br/>
-          <span>A: Enter your long URL and we give you back a short URL!</span>
+          <span>Enter your long URL and we give you back a short URL!</span>
         </div>
         <div>
-          <span>Q: But... can I save this magical links you have create?</span>
+          <span>But... can I save this magical links you have create?</span>
           <br/>
-          <span>A: Yes! The links you made are automatically saved to your browser</span>
+          <span>Yes! The links you made are automatically saved to your browser</span>
         </div>
         <div>
-          <span>Q: But... where do they dwell?</span>
+          <span>But... where do they dwell?</span>
           <br/>
-          <span>A: Simply click on "Links" at the top of the page!</span>
+          <span>Simply click on "Links" at the top of the page!</span>
         </div>
         <div>
-          <span>Q: But... what happens if I lost a link?</span>
+          <span>But... what happens if I lost a link?</span>
           <br/>
-          <span>A: Resubmit the original URL and your link will appear before thine eyes!</span>
+          <span>Resubmit the original URL and your link will appear before thine eyes!</span>
         </div>
       </section>
     </div>
