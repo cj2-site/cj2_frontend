b45df1cd670f2487b0b84c077520cfae2bfbe406
Charles Clemens <cclemensjr@gmail.com>
2019-05-16 20:18:23 -0700

    Updated markup

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -13,9 +13,9 @@ const Faq = () => {
           <span>Enter your long URL and we give you back a short URL!</span>
         </div>
         <div>
-          <span>But... can I save this magical links you have create?</span>
-          <br/>
           <span>Yes! The links you made are automatically saved to your browser</span>
+          <br/>
+          <span>But... can I save this magical links you have create?</span>
         </div>
         <div>
           <span>But... where do they dwell?</span>
@@ -23,9 +23,9 @@ const Faq = () => {
           <span>Simply click on "Links" at the top of the page!</span>
         </div>
         <div>
-          <span>But... what happens if I lost a link?</span>
-          <br/>
           <span>Resubmit the original URL and your link will appear before thine eyes!</span>
+          <br/>
+          <span>But... what happens if I lost a link?</span>
         </div>
       </section>
     </div>
